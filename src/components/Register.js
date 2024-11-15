b01c264224f96526f30ba6ddf039f609603a699a
mansoor <mansoorshaik7b@gmail.com>
2024-11-15 20:55:35 +0530

    user reg notification

--- src/components/Register.js
+++ src/components/Register.js
@@ -26,6 +26,7 @@ const Register = () => {
 
             // Handle the success message here
             setMessage(response.data.msg);
+            console.log("set message: ", response.data.msg)
         } catch (error) {
             console.error('Error registering user:', error.response?.data?.msg || error.message);
             setError('Registration failed: ' + (error.response?.data?.msg || 'Server error'));
